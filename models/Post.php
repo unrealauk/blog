4022bab1b854baa606ba7d169c0318bacee0d45e
auk <i.kvjatkovskij@internetdevels.com>
2017-01-13 12:21:46 +0200

    Added models.

---
+++ models/Post.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $anons
+ * @property string $content
+ * @property integer $category_id
+ * @property integer $author_id
+ * @property string $publish_status
+ * @property string $publish_date
+ *
+ * @property Category $category
+ * @property User $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['anons', 'content', 'publish_status'], 'string'],
+            [['category_id', 'author_id'], 'integer'],
+            [['publish_date'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'anons' => 'Anons',
+            'content' => 'Content',
+            'category_id' => 'Category ID',
+            'author_id' => 'Author ID',
+            'publish_status' => 'Publish Status',
+            'publish_date' => 'Publish Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'author_id']);
+    }
+}
